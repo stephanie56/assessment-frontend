c3890d71239d5fbd0377d6c647fdcbff3822ecf5
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-03-29 17:15:47 -0400

    Restructure sass code

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   isIndexRoute: Ember.computed.equal('currentRouteName', 'products.index'),
   actions: {
-    toggleMenu: () => console.log('Menu is toggled!'),
+    toggleMenu: (val) => console.log(`#{val} is toggled!`),
   }
 });
