9e2eea0445ac2774a222a4d7149865c8c827ebc1
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-03-30 15:32:31 -0400

    Refactor header and top navigation

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isIndexRoute: Ember.computed.equal('currentRouteName', 'products.index'),
+  isShowRoute: Ember.computed.equal('currentRouteName', 'products.show'),
   actions: {
     toggleMenu: (val) => console.log(`#{val} is toggled!`),
   }
